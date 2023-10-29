8ee70da8f4b1fcd5bea7ec87da93b666164e112e
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2023-10-28 21:30:00 -0300

    28-10-2023 v2

--- src/main/java/app/railway/up/controllers/ClienteController.java
+++ src/main/java/app/railway/up/controllers/ClienteController.java
@@ -40,6 +40,12 @@ public class ClienteController {
 
     @GetMapping
     @Operation(summary = "Buscar todos os cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Clientes buscados com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_SUCESSFULLY_FOUND))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para buscar clientes."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))})
+    })
     public ResponseEntity<List<Cliente>> findAll(){
         List<Cliente> clientes = clienteService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(clientes);
@@ -48,6 +54,14 @@ public class ClienteController {
 
     @GetMapping("{id}")
     @Operation(summary = "Buscar um cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Cliente buscado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_SUCESSFULLY_FOUND))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para buscar clientes."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+    })
     public ResponseEntity<Cliente> findById(@PathVariable Long id) throws ResourceNotFoundException {
         Cliente cliente = clienteService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(cliente);
@@ -55,6 +69,14 @@ public class ClienteController {
 
     @PutMapping("{id}")
     @Operation(summary = "Atualizar um cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Cliente atualizado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_UPDATE_CUSTOMER))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para atualizar o cliente."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+    })
     public ResponseEntity<MessageResponseDTO> update(@PathVariable Long id, @RequestBody Cliente cliente) throws ResourceNotFoundException {
         var message = clienteService.update(id,cliente);
         return ResponseEntity.status(HttpStatus.OK).body(message);
@@ -62,13 +84,28 @@ public class ClienteController {
 
     @PatchMapping("{id}/limite")
     @Operation(summary = "Atualizar o limite de um cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Limite atualizado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_UPDATE_VOUCHER))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para atualizar limite do cliente."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+    })
     public ResponseEntity<MessageResponseDTO> patchLimite(@PathVariable Long id, @RequestBody Cliente cliente) throws ResourceNotFoundException {
         var message = clienteService.patchLimite(id,cliente);
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
-
     @PatchMapping("{id}/status")
     @Operation(summary = "Atualizar o status de um cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Status atualizado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_UPDATE_STATUS))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para atualizar status do cliente."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+    })
     public ResponseEntity<MessageResponseDTO> patchStatus(@PathVariable Long id, @RequestBody Cliente cliente) throws ResourceNotFoundException {
         var message = clienteService.patchStatus(id,cliente);
         return ResponseEntity.status(HttpStatus.OK).body(message);
@@ -76,6 +113,14 @@ public class ClienteController {
 
     @DeleteMapping("{id}")
     @Operation(summary = "Deletar um cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Cliente deletado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_DELETE_SUCCESS))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para deletar o cliente."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+    })
     public ResponseEntity<MessageResponseDTO> deleteById(@PathVariable Long id) throws ResourceNotFoundException {
         var message = clienteService.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(message);
@@ -91,5 +136,58 @@ public class ClienteController {
             "  \"status\": 403,\n" +
             "  \"message\": \"Acesso negado.\",\n" +
             "  \"path\": \"/api/v1/clientes\"\n" +
+            "}";
+
+    private final String EXAMPLE_SUCESSFULLY_FOUND = "{\n" +
+            "  \"id\": 1,\n" +
+            "  \"nome\": \"Cliente Teste\",\n" +
+            "  \"email\": \"teste@gmail.com\",\n" +
+            "  \"limite\": 0,\n" +
+            "  \"status\": \"PEN\",\n" +
+            "  \"telefones\": [\n" +
+            "    {\n" +
+            "      \"ddi\": \"55\",\n" +
+            "      \"ddd\": \"31\",\n" +
+            "      \"numero\": \"999999999\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"enderecos\": [\n" +
+            "    {\n" +
+            "      \"endereco\": \"teste\",\n" +
+            "      \"complemento\": \"complemento teste\",\n" +
+            "      \"numero\": \"123\",\n" +
+            "      \"cep\": \"79000000\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+    private final String EXAMPLE_NOT_FOUND = "{\n" +
+            "  \"timestamp\": \"2023-10-28T19:35:52.657+00:00\",\n" +
+            "  \"status\": 404,\n" +
+            "  \"message\": \"Cliente com id 1 não encontrado.\",\n" +
+            "  \"path\": \"/api/v1/clientes/1\"\n" +
+            "}";
+
+    private final String EXAMPLE_UPDATE_CUSTOMER = "\n" +
+            "  \"status\": \"Ok\",\n" +
+            "  \"code\": 200,\n" +
+            "  \"message\": \"Cliente atualizado com sucesso.\"\n" +
+            "}";
+
+    private final String EXAMPLE_UPDATE_VOUCHER = "\n" +
+            "  \"status\": \"Ok\",\n" +
+            "  \"code\": 200,\n" +
+            "  \"message\": \"Limite atualizado com sucesso.\"\n" +
+            "}";
+
+    private final String EXAMPLE_UPDATE_STATUS = "\n" +
+            "  \"status\": \"Ok\",\n" +
+            "  \"code\": 200,\n" +
+            "  \"message\": \"Status atualizado com sucesso.\"\n" +
+            "}";
+
+    private final String EXAMPLE_DELETE_SUCCESS = "\n" +
+            "  \"status\": \"Ok\",\n" +
+            "  \"code\": 200,\n" +
+            "  \"message\": \"Cliente deletado com sucesso.\"\n" +
             "}";
 }
--- src/main/java/app/railway/up/controllers/PedidoController.java
+++ src/main/java/app/railway/up/controllers/PedidoController.java
@@ -5,6 +5,12 @@ import app.railway.up.controllers.dto.request.PedidoDTO;
 import app.railway.up.controllers.exceptions.ResourceNotFoundException;
 import app.railway.up.domain.pedido.Pedido;
 import app.railway.up.service.PedidoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,27 +26,142 @@ public class PedidoController {
     private PedidoService pedidoService;
 
     @PostMapping
+    @Operation(summary = "Criar um novo pedido")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema( description = "Pedido criado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_CREATE_SUCESS))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para criar um pedido."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Cliente ou produto não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_NOT_FOUND))})
+
+    })
     public ResponseEntity<MessageResponseDTO> create(@RequestBody PedidoDTO pedidoDTO) throws ResourceNotFoundException {
         var message = pedidoService.create(pedidoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
     @GetMapping
+    @Operation(summary = "Buscar todos os pedidos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Pedidos buscados com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_SUCESSFULLY_FOUND))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para buscar pedidos."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))})
+    })
     public ResponseEntity<List<Pedido>> findAll() {
         List<Pedido> pedidos = pedidoService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(pedidos);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Buscar um pedido")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Pedido buscado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_SUCESSFULLY_FOUND))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para buscar pedido."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Pedido não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_ORDER_NOT_FOUND))})
+    })
     public ResponseEntity<Pedido> findById(@PathVariable Long id) throws ResourceNotFoundException {
         Pedido pedido = pedidoService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(pedido);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Deletar um pedido")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema( description = "Pedido deletado com sucesso."),
+                    examples = @ExampleObject( value = EXAMPLE_DELETE_SUCCESS))}),
+            @ApiResponse(responseCode = "403", content = { @Content(schema = @Schema( description = "Sem permissão para deletar pedido."),
+                    examples = @ExampleObject( value = EXAMPLE_ACCESS_DENIED))}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema( description = "Pedido não encontrado."),
+                    examples = @ExampleObject( value = EXAMPLE_ORDER_NOT_FOUND))})
+    })
     public ResponseEntity<MessageResponseDTO> deleteById(Long id) throws ResourceNotFoundException {
         var message = pedidoService.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
+
+    private final String EXAMPLE_ORDER_NOT_FOUND = "{\n" +
+                                                   "  \"timestamp\": \"2023-10-28T19:35:52.657+00:00\",\n" +
+                                                   "  \"status\": 404,\n" +
+                                                   "  \"message\": \"Pedido com id 1 não encontrado.\",\n" +
+                                                   "  \"path\": \"/api/v1/pedidos/1\"\n" +
+                                                   "}";
+
+    private final String EXAMPLE_NOT_FOUND = "{\n" +
+                                             "  \"timestamp\": \"2023-10-28T19:35:52.657+00:00\",\n" +
+                                             "  \"status\": 404,\n" +
+                                             "  \"message\": \"Cliente com id 1 não encontrado.\",\n" +
+                                             "  \"path\": \"/api/v1/pedidos\"\n" +
+                                             "},\n" +
+                                             "{\n" +
+                                             "  \"timestamp\": \"2023-10-27T23:25:47.870+00:00\",\n" +
+                                             "  \"status\": 404,\n" +
+                                             "  \"message\": \"Produto com id 1 não encontrado.\",\n" +
+                                             "  \"path\": \"/api/v1/pedidos\"\n" +
+                                             "}";
+
+    private final String EXAMPLE_ACCESS_DENIED = "{\n" +
+                                                 "  \"timestamp\": 1698413037110,\n" +
+                                                 "  \"status\": 403,\n" +
+                                                 "  \"message\": \"Acesso negado.\",\n" +
+                                                 "  \"path\": \"/api/v1/pedidos\"\n" +
+                                                 "}";
+
+    private final String EXAMPLE_CREATE_SUCESS = "{\n" +
+                                                 "  \"status\": \"Created\",\n" +
+                                                 "  \"code\": 201,\n" +
+                                                 "  \"message\": \"Pedido criado com sucesso.\"\n" +
+                                                 "}";
+
+    private final String EXAMPLE_SUCESSFULLY_FOUND = "{\n" +
+                                                     "  \"id\": 1,\n" +
+                                                     "  \"valorTotal\": 999.9,\n" +
+                                                     "  \"dataPedido\": \"2023-10-28T20:45:28.148952\",\n" +
+                                                     "  \"items\": [\n" +
+                                                     "    {\n" +
+                                                     "      \"produto\": {\n" +
+                                                     "        \"id\": 1,\n" +
+                                                     "        \"nome\": \"produto teste\",\n" +
+                                                     "        \"preco\": 99.99,\n" +
+                                                     "        \"ean\": \"123456789\",\n" +
+                                                     "        \"qtUnit\": 1,\n" +
+                                                     "        \"estoque\": 89\n" +
+                                                     "      },\n" +
+                                                     "      \"quantidade\": 10\n" +
+                                                     "    }\n" +
+                                                     "  ],\n" +
+                                                     "  \"cliente\": {\n" +
+                                                     "    \"id\": 3,\n" +
+                                                     "    \"nome\": \"Cliente teste\",\n" +
+                                                     "    \"email\": \"teste@gmail.com\",\n" +
+                                                     "    \"limite\": 0,\n" +
+                                                     "    \"status\": \"PEN\",\n" +
+                                                     "    \"telefones\": [\n" +
+                                                     "      {\n" +
+                                                     "        \"ddi\": \"55\",\n" +
+                                                     "        \"ddd\": \"66\",\n" +
+                                                     "        \"numero\": \"977777777\"\n" +
+                                                     "      }\n" +
+                                                     "    ],\n" +
+                                                     "    \"enderecos\": [\n" +
+                                                     "      {\n" +
+                                                     "        \"endereco\": \"teste\",\n" +
+                                                     "        \"complemento\": \"complemento teste\",\n" +
+                                                     "        \"numero\": \"1234\",\n" +
+                                                     "        \"cep\": \"99900000\"\n" +
+                                                     "      }\n" +
+                                                     "    ]\n" +
+                                                     "  }\n" +
+                                                     "}";
+
+    private final String EXAMPLE_DELETE_SUCCESS = "\n" +
+            "  \"status\": \"Ok\",\n" +
+            "  \"code\": 200,\n" +
+            "  \"message\": \"Pedido deletado com sucesso.\"\n" +
+            "}";
 
 }
